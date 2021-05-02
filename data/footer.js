e07e5581e5563bfffca1309f74ea2e403cfa3139
Thomas Sankara <thomassankara74@gmail.com>
2021-05-02 12:42:21 +0300

    footer data array

---
+++ data/footer.js
@@ -0,0 +1,32 @@
+export const footer = [
+  {
+    id: 1,
+    title: "Facebook",
+    url: "https://facebook.com/tsbsankara",
+  },
+  {
+    id: 2,
+    title: "Instagram",
+    url: "https://instagram.com/tsbsankara",
+  },
+  {
+    id: 3,
+    title: "Twitter",
+    url: "https://twitter.com/TSBsankara",
+  },
+  {
+    id: 4,
+    title: "YouTube",
+    url: "https://youtube.com/tsbsankara",
+  },
+  {
+    id: 5,
+    title: "Udemy",
+    url: "https://www.udemy.com/user/thomas-sankara-babu/",
+  },
+  {
+    id: 6,
+    title: "Gumroad",
+    url: "https://gumroad.com/tsbsankara",
+  },
+]
